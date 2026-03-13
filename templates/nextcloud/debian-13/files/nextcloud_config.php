063804bc43016f7c97a3466cc687d2e645f407af
Matthew Horwood <matt@horwood.biz>
2026-03-13 08:34:15 +0000

    Update nextcloud_config.php
    bug: blank config

--- templates/nextcloud/debian-13/files/nextcloud_config.php
+++ templates/nextcloud/debian-13/files/nextcloud_config.php
@@ -1,5 +1,22 @@
 <?php
 $CONFIG = array (
+  'installed' => false,
   'serverid' => 1,
+  'instanceid' => '',
+  'passwordsalt' => '',
+  'secret' => '',
+  'trusted_domains' => [
+    'demo.example.org',
+    'otherdomain.example.org',
+    '10.111.112.113',
+    '[2001:db8::1]'
+  ],
   'datadirectory' => '/var/lib/nextcloud/',
-}
+  'dbtype' => 'sqlite3',
+  'version' => '',
+  'dbname' => '',
+  'dbhost' => '',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => '',
+  'dbpassword' => '',
+);
